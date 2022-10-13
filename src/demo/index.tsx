4bb768db3940c6e279fcaeed610c7052cb89f49b
张亚飞 <1739077662@qq.com>
2022-10-13 16:58:34 +0800

    init

---
+++ src/demo/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Index({}: Props) {
+  return <div>Index</div>;
+}
+
+export default Index;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import App from "./demo";
+
+ReactDom.render(<App />, document.querySelector("#app"));
